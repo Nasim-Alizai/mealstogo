702547e87a2057b5bca6875c97788ec6742ed346
Nasim-Alizai <sayedhakimi449@gmail.com>
2022-11-23 13:32:34 +0430

    add menu drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mealstogo/screens/filters_screen.dart';
 import './screens/tabs_screen.dart';
 import './screens//tabs_bottom_screen.dart';
 import './screens/meal_detail_screen.dart';
@@ -47,6 +48,7 @@ class MyApp extends StatelessWidget {
         '/': (ctx) => TabsBottomScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+        FiltersScreen.routeName: (ctx) => FiltersScreen(),
       },
     );
   }
